a2e91eadaae2cd0f84b8c202ce857e05f80a4987
JackDownload <kismocsy@gmail.com>
2021-08-23 16:03:48 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,8 @@
+import pkg from './package'
+import info from './content/setup/info'
+import path from 'path'
+import glob from 'glob'
+
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -23,74 +28,55 @@ export default {
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap' },
     ],
-    script: [
-      { src: 'https://polyfill.io/v3/polyfill.min.js?features=es6', body: true },
-    ]
-  },
+script: [
+  { src: 'https://polyfill.io/v3/polyfill.min.js?features=es6', body: true },
+]
+},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/scss/style.scss',
-  ],
+// Global CSS: https://go.nuxtjs.dev/config-css
+css: [
+'@/assets/scss/style.scss',
+],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/bootstrap.client.js' }
-  ],
+// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+plugins: [
+{ src: '~/plugins/bootstrap.client.js' }
+],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+// Auto import components: https://go.nuxtjs.dev/config-components
+components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+buildModules: [
+],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/style-resources',
-    '@nuxtjs/sitemap',
-    '@nuxtjs/gtm',
-  ],
+// Modules: https://go.nuxtjs.dev/config-modules
+modules: [
+// https://go.nuxtjs.dev/pwa
+'@nuxtjs/pwa',
+// https://go.nuxtjs.dev/content
+'@nuxt/content',
+'bootstrap-vue/nuxt',
+'@nuxtjs/style-resources',
+'@nuxtjs/sitemap',
+'@nuxtjs/gtm',
+],
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
-  pwa: {
-    manifest: {
-      lang: 'en'
-    }
-  },
-
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {},
-  sitemap: {
-    hostname: 'https://elegant-mahavira-21b6ba.netlify.app',
-    gzip: true,
-    routes: async () => {
-      const { $content } = require('@nuxt/content')
-      const articles = await $content('articles').only(['path']).fetch()
-      return articles.map((p) => p.path)
-    }
-  },
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    loaders: {
-      vue: {
-        compilerOptions: {
-          whitespace: 'condense',
-          preserveWhitespace: false
-        }
-      },
-      sass: {
-        implementation: require('sass')
-      },
-      scss: {
-        implementation: require('sass')
-      }
-    }
-  }
+// PWA module configuration: https://go.nuxtjs.dev/pwa
+pwa: {
+manifest: {
+  lang: 'en'
 }
+},
+
+// Content module configuration: https://go.nuxtjs.dev/config-content
+content: {},
+sitemap: {
+hostname: 'https://elegant-mahavira-21b6ba.netlify.app',
+gzip: true,
+routes: async () => {
+  const { $content } = require('@nuxt/content')
+  const articles = await $content('articles').only(['path']).fetch()
+  return articles.map((p) => p.path)
+}
+},
